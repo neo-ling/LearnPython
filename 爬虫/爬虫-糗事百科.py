4ef3e39e071cbaa11d50bf5543bdd0275b55a826
neo-ling <18683130956@163.com>
2015-11-19 21:56:29 +0800

    爬虫-糗事百科

---
+++ 爬虫/爬虫-糗事百科.py
@@ -0,0 +1,22 @@
+#coding=utf-8
+import urllib
+import urllib2
+import re
+page = 1
+url = "http://www.qiushibaike.com/hot/page/" + str(page)
+user_agent = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36"
+headers = {'User-Agent':"user_agent"}
+try:
+    request = urllib2.Request(url,headers = headers)
+    response = urllib2.urlopen(request)
+    print response.read()
+    content = response.read().decode('utf-8')
+    pattern  = re.compile('<h2>(.*?)</h2>.*?<div class="content">(.*?)<!--(.*?)--></div>',re.S)
+    items = re.findall(pattern,content)
+    for item in items:
+        print item[0],item[1],item[2]
+except urllib2.URLError,e:
+    if hasattr(e,"code"):
+        print e.code
+    if hasattr(e,"reason"):
+        print e.reason
